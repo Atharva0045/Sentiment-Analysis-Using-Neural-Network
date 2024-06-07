4aa6250f821aa6dc75d2ace5d6bc97d291b45a9e
Atharva Aher <atharva758876@gmail.com>
2024-06-07 17:09:42 +0530

    reverted back to the original

--- deployment.py
+++ deployment.py
@@ -33,3 +33,7 @@ def predict_sentiment(headline):
     predicted_sentiment = sentiment_labels[np.argmax(prediction)]
     
     return predicted_sentiment
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
